be1ad58c5aebb1b5a97ddc98a36ebbbe46b5f2c4
sabakhann64 <100476160+sabakhann64@users.noreply.github.com>
2022-02-27 23:14:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,9 @@ function compute()
     r = document.getElementById("rate").value;
     i = document.getElementById("years").value;    
     var SI = p*r*i;
-    var text = "";
     const d = new Date();
     let year = d.getFullYear();
-    text = document.setElementByinnerHTML("If you deposit $(p), at an interest rate of $(rate)%. You will receive an amount of $(SI), in the year $(year)");
+    document.setElementById("result").innerHTML = "If you deposit ${p)"
     
 }
         
