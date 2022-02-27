aafa918f79aed50a89a179abc5316a199cb52bc1
sabakhann64 <100476160+sabakhann64@users.noreply.github.com>
2022-02-27 23:04:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    i = document.getElementById("years").value;    
+    var SI = p*r*i;
+    var text = "";
+    const d = new Date();
+    let year = d.getFullYear();
+    text = document.setElementByinnerHTML("If you deposit $(p), at an interest rate of $(rate)%. You will receive an amount of $(SI), in the year $(year)");
     
 }
         
