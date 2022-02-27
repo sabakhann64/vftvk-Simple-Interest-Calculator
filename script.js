1b179b44e3cc951f43174a69a6a0127863d80146
sabakhann64 <100476160+sabakhann64@users.noreply.github.com>
2022-02-27 23:31:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function compute()
     var SI = ((p*r*i)/100);
     const d = new Date();
     let year = d.getFullYear();
-    document.getElementById("result").innerHTML = "If you deposit $("p")"
+    document.getElementById("result").innerHTML = "saba"
     
 }
         
